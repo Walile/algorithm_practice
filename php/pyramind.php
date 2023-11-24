09201e43505c9a7997cb85a572bae8fba3b7c803
Doris Wang (Walile) <fance26@gmail.com>
2023-11-24 15:08:17 +0800

    finish pyramind solve

---
+++ php/pyramind.php
@@ -0,0 +1,38 @@
+<?php
+
+function pyramind($n) {
+    $pyramind = array();
+    // if (($n/2) == 0){
+    //     return "Please enter an odd number";
+    // }
+
+    $midKey = $n;
+    $rowRange = $midKey + $n;
+    
+    // Create the pyramind
+    foreach(range(0, $n) as $row){
+        foreach(range(0, $rowRange) as $value){
+            $pyramind[$row][$value] = ' ';
+            if ($value == $midKey){
+                $pyramind[$row][$value] = '*';
+            }
+            if ($value >= ($midKey - $row) && $value <= $midKey + $row){
+                $pyramind[$row][$value] = '*';
+            }
+        }
+    }
+    return $pyramind;
+}
+
+
+function printPyramind($pyramind) {
+    foreach ($pyramind as $row) {
+        foreach ($row as $col) {
+            echo $col;
+        }
+        echo "\n";
+    }
+}
+
+$pyramind = pyramind(10);
+printPyramind($pyramind);
